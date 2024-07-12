95beaa80595d9882894c4ee140a090d1e58d4bd7
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-12 15:11:43 +0200

    feat: layout basic for header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<script>
+export default{
+    name:'AppHeader',
+}
+</script>
+
+
+<template>
+    <header>
+        <nav>
+            <div id="logo">
+                <a href="#"><h1>BOOLFLIX</h1></a>
+            </div>
+            <div id="search_bar">
+                <input type="text">
+            </div>
+        </nav>
+    </header>
+
+</template>
+
+<style lang="scss">
+nav{
+    background-color: black;
+    height: 70px;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+        h1{
+            color: red;
+        }
+}
+   
+
+</style>
